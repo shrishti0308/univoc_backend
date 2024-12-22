1abd8ad8f958097e72a16e580d765f6bb23eeb58
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2024-12-22 21:34:04 +0530

    add database connection setup with mongoose and dotenv

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
+
+connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
