cc55051cbeb0d97f13030dc0509a33cdf8fab440
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2024-12-22 22:07:24 +0530

    add dummy data for Electrical and Electronics course and update package dependencies

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const connectDB = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.use(errorHandler);
+app.use("/api/courses", require("./routes/courseRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
